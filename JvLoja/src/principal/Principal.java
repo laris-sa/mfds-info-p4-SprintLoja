46369fefb2ab9cf800922a66e41440a3bed62553
larissa <95726877+larissahol@users.noreply.github.com>
2023-05-13 19:27:55 -0300

    Update Principal.java

--- JvLoja/src/principal/Principal.java
+++ JvLoja/src/principal/Principal.java
@@ -4,8 +4,8 @@ import criarConta.CriarConta;
 
 public class Principal {
 	    public static void main(String[] args) {
-	        CriarConta criarConta = new CriarConta();  // Cria um objeto da classe CriarConta
-	        criarConta.main(args);  // Chama o método main da classe CriarConta
+	        CriarConta criarConta = new CriarConta();
+	        criarConta.main(args); 
 	    }
 	}
 
