67529eee8a95c8f83154cdafd3e2e1e37d63e5d8
larissa <95726877+larissahol@users.noreply.github.com>
2023-05-13 19:26:15 -0300

    Add files via upload

---
+++ JvLoja/src/principal/Principal.java
@@ -0,0 +1,13 @@
+package principal;
+
+import criarConta.CriarConta;
+
+public class Principal {
+	    public static void main(String[] args) {
+	        CriarConta criarConta = new CriarConta();  // Cria um objeto da classe CriarConta
+	        criarConta.main(args);  // Chama o método main da classe CriarConta
+	    }
+	}
+
+
+
